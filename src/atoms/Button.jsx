ccdb45dac9811c7ff80b4498098fc41793ae5df2
Masuda-1246 <yaojiezengtian38@gmail.com>
2022-08-25 00:24:29 +0900

    create atoms button

---
+++ src/atoms/Button.jsx
@@ -0,0 +1,28 @@
+import { Link } from 'react-router-dom';
+
+const CreateButton = (props) => {
+    const text = props.text;
+    const link = props.link;
+    const button = {
+        alignItems:"left",
+        padding: "15px",
+        paddingLeft: "50px",
+        paddingRight: "150px",
+        fontSize: "16px",
+        color: "#1A4F83",
+        border: "none",
+        borderRadius: "5px",
+        backgroundColor: "white",
+        boxShadow: "0px 2px 10px rgba(26, 79, 131, .1)",
+    };
+
+    return (
+        <div>
+            <Link to={link}>
+                <button style={button}>{text}</button>
+            </Link>
+        </div>
+    );
+};
+
+export default CreateButton;
--- src/atoms/index.js
+++ src/atoms/index.js
@@ -1,2 +1,3 @@
 export {default as CreateRadarCharts } from './RadarChart';
 export {default as CreateSlider } from './Slider';
+export {default as CreateButton } from './Button';
