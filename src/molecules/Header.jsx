88a363212b1642326315f4e63bd4f5cbdc8dea65
riiiiin <risam1868@gmail.com>
2022-08-24 19:30:02 +0900

    create-molecules-Header

---
+++ src/molecules/Header.jsx
@@ -0,0 +1,14 @@
+const Header = () => {
+    const header = {
+        color: "#43CBC3",
+        fontWeight: "bold",
+        fontSize: "20px",
+        textAlign: "center"
+    };
+
+    return (
+        <div style={header}>career passport</div>
+    );
+};
+
+export default Header;
