7ad8ae9d3e8a9d4af25ed8ec83b84d13025c7162
riiiiin <risam1868@gmail.com>
2022-08-26 03:32:09 +0900

    design-SigunUp

--- src/pages/SignUp.jsx
+++ src/pages/SignUp.jsx
@@ -1,7 +1,8 @@
 import { auth } from '../firebase';
 import { createUserWithEmailAndPassword, sendEmailVerification } from 'firebase/auth';
 import { useState } from 'react';
-import { Link, useNavigate } from 'react-router-dom';
+import { useNavigate } from 'react-router-dom';
+import { Header } from '../molecules/index';
 
 const SignUp = () => {
     const navigate = useNavigate();
@@ -18,7 +19,7 @@ const SignUp = () => {
         if (name.value.length === 0) {
             setError('ユーザー名を入力してください');
             return;
-        }
+        };
         try {
             await createUserWithEmailAndPassword(auth, email.value, password.value).then(()=>{
                 sendEmailVerification(auth.currentUser);
@@ -39,40 +40,117 @@ const SignUp = () => {
                     error_message = "このメールアドレスは既に使用されています";
                 } else {
                     error_message = e.message;
-                }
+                };
                     setError(error_message);
                 })
         } catch (error) {
             console.log(error.message);
-        }
-    }
+        };
+    };
+
+    const title = {
+        margin: "0 50px",
+        color: "rgba(26, 79, 131, .75)",
+        fontSize: "22px"
+    };
+    const form = {
+        marginTop: "50px",
+        display: "flex",
+        flexDirection: "column",
+        justifyContent: "center",
+        alignItems: "center"
+    };
+    const schoolInfo = {
+        margin: "40px 0",
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center"
+    };
+    const label = {
+        color: "rgba(26, 79, 131, .75)",
+        fontSize: "10px"
+    };
+    const pullDown = {
+        textAlign: "center",
+        width: "80px",
+        height: "30px"
+    };
+    const userInfo = {
+        marginBottom: "30px",
+        display: "flex",
+        flexDirection: "column",
+        justifyContent: "center",
+        alignItems: "center"
+    };
+    const input = {
+        width: "260px",
+        border: "none",
+        borderBottom: "1px solid rgba(26, 79, 131, .5)"
+    };
+    const button = {
+        marginTop: "10px",
+        color: "white",
+        padding: "10px 100px",
+        backgroundColor: "#43CBC3",
+        border: "none",
+        borderRadius: "20px"
+    };
 
     return(
         <div>
-            <h1>SignUp</h1>
-            {error && <p style={{color:"red"}}>{error}</p>}
-            <form onSubmit={handleSubmit}>
-                <div>
-                    <label>ユーザー名</label>
-                    <input name="name" type="text" placeholder="name" />
-                </div>
-                <div>
-                    <label>メールアドレス</label>
-                    <input name="email" type="email" placeholder="email" />
-                </div>
-                <div>
-                    <label>パスワード</label>
-                    <input name="password" type="password" placeholder="password" />
-                </div>
-                <div>
-                    <button>register</button>
-                </div>
-            </form>
+            <Header />
             <div>
-                アカウントをお持ちの方は<Link to={'/signin'}>こちら</Link>から
+                {error && <p style={{color:"red",marginLeft:"50px"}}>{error}</p>}
+                <form style={form} onSubmit={handleSubmit}>
+                    <div style={title}>ユーザー登録</div>
+                    <div style={schoolInfo}>
+                        <div style={{margin:"0 5px"}}>
+                            <div style={label}>学年</div>
+                            <select style={pullDown} name="prefecture">
+                                <option></option>
+                                <option>1</option>
+                                <option>1</option>
+                                <option>1</option>
+                            </select>
+                        </div>
+                        <div style={{margin:"0 5px"}}>
+                            <div style={label}>組</div>
+                            <select style={pullDown} name="prefecture">
+                                <option></option>
+                                <option>1</option>
+                                <option>1</option>
+                                <option>1</option>
+                            </select>
+                        </div>
+                        <div style={{margin:"0 5px"}}>
+                            <div style={label}>番号</div>
+                            <select style={pullDown} name="prefecture">
+                                <option></option>
+                                <option>1</option>
+                                <option>1</option>
+                                <option>1</option>
+                            </select>
+                        </div>
+                    </div>
+                    <div style={userInfo}>
+                        <div style={{marginBottom: "13px"}}>
+                            <div style={label}>氏名</div>
+                            <input style={input} name="name" type="text"></input>
+                        </div>
+                        <div style={{marginBottom: "13px"}}>
+                            <div style={label}>メールアドレス</div>
+                            <input style={input} name="email" type="email"></input>
+                        </div>
+                        <div style={{marginBottom: "13px"}}>
+                            <div style={label}>パスワード</div>
+                            <input style={input} name="password" type="password"></input>
+                        </div>
+                    </div>
+                    <button style={button}>次へ</button>
+                </form>
             </div>
         </div>
-    )
-}
+    );
+};
 
 export default SignUp;
