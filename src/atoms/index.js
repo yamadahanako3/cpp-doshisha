3b042dc5580f7fd2477dd2d80d204973085fc1f5
Masuda-1246 <yaojiezengtian38@gmail.com>
2022-08-25 17:47:21 +0900

    create to publish button

--- src/atoms/index.js
+++ src/atoms/index.js
@@ -2,3 +2,4 @@ export {default as RadarChart } from './RadarChart';
 export {default as CreateSlider } from './Slider';
 export {default as CreateButton } from './Button';
 export {default as DoughnutChart } from './DoughnutChart';
+export {default as ToPublishButton } from './ToPublishButton';
