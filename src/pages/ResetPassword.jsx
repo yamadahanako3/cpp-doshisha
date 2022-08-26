4ca0be976c1248237e45144786aab9a1b3822702
riiiiin <risam1868@gmail.com>
2022-08-26 18:19:41 +0900

    design-ResetPassword

--- src/pages/ResetPassword.jsx
+++ src/pages/ResetPassword.jsx
@@ -1,8 +1,10 @@
 import { sendPasswordResetEmail } from 'firebase/auth';
 import { auth } from '../firebase';
-import { useNavigate } from 'react-router-dom'
+import { useNavigate } from 'react-router-dom';
+import { Header } from '../molecules/index';
 
 const ResetPassword = () => {
+  const height = window.innerHeight + "px";
   const navigate = useNavigate();
 
   const handleSubmit = async (event) => {
@@ -15,16 +17,66 @@ const ResetPassword = () => {
     });
   };
 
+  const title = {
+    marginTop: "100px",
+    color: "rgba(26, 79, 131, .75)",
+    fontSize: "22px"
+  };
+  const underTitle = {
+    marginBottom: "100px",
+    color: "rgba(26, 79, 131, .5)",
+    fontSize: "13px",
+  }
+  const form = {
+    height: height,
+    display: "flex",
+    flexDirection: "column",
+    alignItems: "center"
+  };
+  const label = {
+    color: "rgba(26, 79, 131, .75)",
+    fontSize: "15px"
+  };
+  const userInfo = {
+    marginBottom: "30px",
+    display: "flex",
+    flexDirection: "column",
+    justifyContent: "center",
+    alignItems: "center"
+  };
+  const input = {
+    marginBottom: "30px",
+    width: "260px",
+    height: "50px",
+    border: "none",
+    borderBottom: "1px solid rgba(26, 79, 131, .5)",
+  };
+  const button = {
+    marginTop: "10px",
+    fontSize: "15px",
+    color: "white",
+    padding: "10px 100px",
+    backgroundColor: "#43CBC3",
+    border: "none",
+    borderRadius: "20px"
+  };
+
   return (
     <div>
-      <h1>パスワード再設定</h1>
-      <form onSubmit={handleSubmit}>
+      <Header />
         <div>
-          <label>メールアドレス</label>
-          <input type='email' name='email' placeholder='email'/>
+          <form style={form} onSubmit={handleSubmit}>
+            <div style={title}>パスワード再設定</div>
+            <p style={underTitle}>パスワード再設定用メールを送信します。</p>
+            <div style={userInfo}>
+                <div style={{marginBottom: "13px"}}>
+                    <div style={label}>メールアドレス</div>
+                    <input style={input} name="email" type="email"></input>
+                </div>
+            </div>
+            <button style={button}>送信</button>
+          </form>
         </div>
-        <button>送信</button>
-      </form>
     </div>
   );
 };
